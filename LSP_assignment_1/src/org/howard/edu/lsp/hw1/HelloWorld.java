a7826da8b63df7311aedfebbe8bcecab3022a76a
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-01-19 23:03:28 -0500

    This is Assignment #1.

---
+++ LSP_assignment_1/src/org/howard/edu/lsp/hw1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.hw1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
---
+++ LSP_assignment_1/src/org/howard/edu/lsp/hw2/HelloWorld2.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.hw2;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
