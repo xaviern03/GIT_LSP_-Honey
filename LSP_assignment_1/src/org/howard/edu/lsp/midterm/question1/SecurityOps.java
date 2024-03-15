f665a1fee883b427bbb0a6601b874d0034616b85
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-03-14 20:57:35 -0400

    Added comments to class

--- LSP_assignment_1/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -5,15 +5,18 @@ public class SecurityOps {
 	public static String encrypt(String text) {
 		StringBuilder evenChars = new StringBuilder();
 		StringBuilder oddChars = new StringBuilder();
-		
+		// Loop through the text, character by character.
 		for (int i = 0; i < text.length(); i++) {
 			if (i % 2 == 0) {
+				// Even-indexed characters (considering 0 as the first index)
 				evenChars.append(text.charAt(i));
 			} else {
+				// Odd-indexed characters
 				oddChars.append(text.charAt(i));
 			}
 		}
 		
+		// Combine even-indexed and odd-indexed characters.
 		return evenChars.toString() + oddChars.toString();
 	}
 
