0895066f001d140bfd959615d21cafba1cf54faf
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-03-14 20:54:49 -0400

    Same as previous commits, did not update code for the initial commit.

--- LSP_assignment_1/src/org/howard/edu/lsp/midterm/question2/IntegerRange.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/midterm/question2/IntegerRange.java
@@ -1,5 +1,53 @@
 package org.howard.edu.lsp.midterm.question2;
 
-public class IntegerRange {
-
+public class IntegerRange implements Range {
+	private int lowerBound;
+	private int upperBound;
+	
+	public IntegerRange(int lower, int upper) {
+		this.lowerBound = lower;
+		this.upperBound = upper;
+	}
+	
+	/**
+	 * Returns true if value is greater than or equal to lower bound and less than or equal to upper bound.
+	 */
+	
+	public boolean contains(int value) {
+		return value >= lowerBound && value <= upperBound;
+	}
+	
+	/**
+	 * Returns true if this range contains at least one value in common with the other range.
+	 */
+	public boolean overlaps(Range other) {
+		if (other instanceof IntegerRange) {
+			IntegerRange otherRange = (IntegerRange) other;
+			return this.lowerBound <= otherRange.upperBound && this.upperBound >= otherRange.lowerBound;
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the number of integers in the range.
+	 */
+	
+	public int size() {
+		return upperBound - lowerBound + 1;
+	}
+	
+	/**
+	 * Two Ranges are equal if their lower and upper bounds are the same.
+	 */
+	
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass()!= obj.getClass()) return false;
+		IntegerRange that = (IntegerRange) obj;
+		return lowerBound == that.lowerBound && upperBound == that.upperBound;
+	}
+	
+	public int hashCode() {
+		return 31 * lowerBound + upperBound;
+	}
 }
