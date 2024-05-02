026e5dfbe2fd4ccbf6d3861d9d1773fac7a95612
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 19:31:27 -0400

    SongDatabase Class and JUnit Test

---
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongDatabaseTest.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.oopfinal.question1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SongDatabaseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongsDatabase.java
@@ -0,0 +1,5 @@
+package org.howard.edu.lsp.oopfinal.question1;
+
+public class SongsDatabase {
+
+}
