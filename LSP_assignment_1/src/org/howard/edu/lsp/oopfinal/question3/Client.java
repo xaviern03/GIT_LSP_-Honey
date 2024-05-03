fe1c3ff1165bc500051c2f8f8560d92513bbba18
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:33:04 -0400

    Update Client.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Client.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Client.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lsp.oopfinal.question3;
 
 public class Client {
-
+	public static void main(String[] args) {
+        Shape circle = ShapeFactory.getShape("CIRCLE");
+        circle.draw();
+        
+        Shape rectangle = ShapeFactory.getShape("RECTANGLE");
+        rectangle.draw();
+    }
 }
