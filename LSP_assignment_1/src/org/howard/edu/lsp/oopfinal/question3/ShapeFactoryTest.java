b38f414486248b7cf7750ff518a0f540cdeb596b
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:34:11 -0400

    Update ShapeFactoryTest.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactoryTest.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactoryTest.java
@@ -2,13 +2,38 @@ package org.howard.edu.lsp.oopfinal.question3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ShapeFactoryTest {
+public class ShapeFactoryTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testCircleCreation() {
+        Shape circle = ShapeFactory.getShape("CIRCLE");
+        circle.draw();
+        assertEquals("I’m a Circle!!!", outContent.toString().trim());
+    }
+
+    @Test
+    public void testRectangleCreation() {
+        Shape rectangle = ShapeFactory.getShape("RECTANGLE");
+        rectangle.draw();
+        assertEquals("I’m a Rectangle!!!", outContent.toString().trim());
+    }
 }
