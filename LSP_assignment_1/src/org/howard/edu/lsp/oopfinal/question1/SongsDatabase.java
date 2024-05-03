346de09c88a1f8d234a0c1e3b9b75321b6592cd8
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:30:44 -0400

    Update SongsDatabase.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongsDatabase.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongsDatabase.java
@@ -1,5 +1,31 @@
 package org.howard.edu.lsp.oopfinal.question1;
 
-public class SongsDatabase {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class SongsDatabase {
+	private Map<String, Set<String>> map = new HashMap<>();
+
+    /* Add a song title to a genre */
+    public void addSong(String genre, String songTitle) {
+        map.putIfAbsent(genre, new HashSet<>());
+        map.get(genre).add(songTitle);
+    }
+
+    /* Return genre, i.e., jazz, given a song title */
+    public String getGenreOfSong(String songTitle) {
+        for (String genre : map.keySet()) {
+            if (map.get(genre).contains(songTitle)) {
+                return genre;
+            }
+        }
+        return null;  // return null if the song title is not found in any genre
+    }
+
+    /* Return the Set that contains all songs for a genre */
+    public Set<String> getSongs(String genre) {
+        return map.getOrDefault(genre, new HashSet<>());
+    }
 }
