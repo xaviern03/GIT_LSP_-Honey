4605ca728278f1c78e0dff8fedc8017bcb6a81e2
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:30:00 -0400

    Manually added copied code from Eclipse due to error commiting.
    Committed in eclipse but code didn't appear so copied manually.

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongDatabaseTest.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question1/SongDatabaseTest.java
@@ -3,12 +3,38 @@ package org.howard.edu.lsp.oopfinal.question1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 
 class SongDatabaseTest {
+	   private SongsDatabase db;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	    @BeforeEach
+	    void setUp() {
+	        db = new SongsDatabase();
+	        db.addSong("Rap", "Savage");
+	        db.addSong("Country", "Sweet Alabama");
+	        db.addSong("Jazz", "Always There");
+	    }
+
+	    @Test
+	    void testAddSong() {
+	        db.addSong("Rap", "Gin and Juice");
+	        assertTrue(db.getSongs("Rap").contains("Gin and Juice"));
+	    }
+
+	    @Test
+	    void testGetGenreOfSong() {
+	        assertEquals("Rap", db.getGenreOfSong("Savage"));
+	        assertEquals("Jazz", db.getGenreOfSong("Always There"));
+	        assertNull(db.getGenreOfSong("Unknown Song"));
+	    }
+
+	    @Test
+	    void testGetSongs() {
+	        Set<String> rapSongs = db.getSongs("Rap");
+	        assertTrue(rapSongs.contains("Savage"));
+	        assertEquals(1, rapSongs.size());  // Check the number of songs in the Rap genre
+	    }
 
 }
