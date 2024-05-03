5f2bce041c17304158e0d140ce27ba5d82be0f79
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:31:09 -0400

    Update BitcoinPayment.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/BitcoinPayment.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/BitcoinPayment.java
@@ -1,5 +1,14 @@
 package org.howard.edu.lsp.oopfinal.question2;
 
-public class BitcoinPayment {
+public class BitcoinPayment implements PaymentStrategy {
+    private String bitcoinAddress;
 
+    public BitcoinPayment(String bitcoinAddress) {
+        this.bitcoinAddress = bitcoinAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Bitcoin address " + bitcoinAddress);
+    }
 }
