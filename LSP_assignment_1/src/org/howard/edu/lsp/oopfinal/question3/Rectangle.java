94a0d5be5753137a19cedec0f609052dc8f4a3e4
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:33:19 -0400

    Update Rectangle.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Rectangle.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Rectangle.java
@@ -1,5 +1,8 @@
 package org.howard.edu.lsp.oopfinal.question3;
 
-public class Rectangle {
-
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("I’m a Rectangle!!!");
+    }
 }
