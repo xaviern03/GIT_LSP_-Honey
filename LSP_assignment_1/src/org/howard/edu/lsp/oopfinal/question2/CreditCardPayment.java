e63fb4f1ac2f1b79307b77d95da75b0b77777640
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:31:28 -0400

    Update CreditCardPayment.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/CreditCardPayment.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/CreditCardPayment.java
@@ -1,5 +1,14 @@
 package org.howard.edu.lsp.oopfinal.question2;
 
-public class CreditCardPayment {
+public class CreditCardPayment implements PaymentStrategy {
+    private String creditCardNumber;
 
+    public CreditCardPayment(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using credit card " + creditCardNumber);
+    }
 }
