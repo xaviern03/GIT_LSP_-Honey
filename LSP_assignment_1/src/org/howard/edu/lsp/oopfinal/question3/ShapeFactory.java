2083658bedeac31c67d963965be56f19aeb469b1
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:33:55 -0400

    Update ShapeFactory.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactory.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactory.java
@@ -1,5 +1,16 @@
 package org.howard.edu.lsp.oopfinal.question3;
 
 public class ShapeFactory {
-
+    // Factory method to create objects of type Shape
+    public static Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        }
+        return null;
+    }
 }
