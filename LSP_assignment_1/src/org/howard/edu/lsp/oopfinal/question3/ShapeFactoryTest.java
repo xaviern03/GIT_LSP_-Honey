77697e66b49f97fe32d11c73faca2a1b78045870
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:28:51 -0400

    Shape Factory JUnit Tests

---
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactoryTest.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.oopfinal.question3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShapeFactoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
