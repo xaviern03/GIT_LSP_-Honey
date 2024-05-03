9f884da10c3e9c22de3c3f6d7dd33bf283c6ef59
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:32:42 -0400

    Update Circle.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Circle.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question3/Circle.java
@@ -1,5 +1,8 @@
 package org.howard.edu.lsp.oopfinal.question3;
 
-public class Circle {
-
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("I’m a Circle!!!");
+    }
 }
