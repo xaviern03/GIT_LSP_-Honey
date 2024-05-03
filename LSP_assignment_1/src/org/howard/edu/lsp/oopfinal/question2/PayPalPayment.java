8b9af2cd9a98b1d7c569bed23c3843080a11e403
xaviern03 <135077216+xaviern03@users.noreply.github.com>
2024-05-02 20:31:52 -0400

    Update PayPalPayment.java

--- LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/PayPalPayment.java
+++ LSP_assignment_1/src/org/howard/edu/lsp/oopfinal/question2/PayPalPayment.java
@@ -1,5 +1,14 @@
 package org.howard.edu.lsp.oopfinal.question2;
 
-public class PayPalPayment {
+public class PayPalPayment implements PaymentStrategy {
+    private String emailAddress;
 
+    public PayPalPayment(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using PayPal account " + emailAddress);
+    }
 }
